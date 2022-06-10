e23374bb8a71432359bd467045cc6d36329a729e
LYK <dalinaum@gmail.com>
2022-06-10 18:43:57 +0900

    respnse_status_code.py 추가

---
+++ week2/response_status_code.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, status
+
+app = FastAPI()
+
+
+# @app.post("/items/", status_code=201)
+# async def create_item(name: str):
+#     return {"name": name}
+
+@app.post("/items/", status_code=status.HTTP_201_CREATED)
+async def create_item(name: str):
+    return {"name": name}
